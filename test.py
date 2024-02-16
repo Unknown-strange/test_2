92d9e6181bda9eb7a67fd24739074e8f7a98419e
The_Unknown <128997233+Unknown-strange@users.noreply.github.com>
2024-02-16 15:44:50 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-current_users = ["Prince","Edwin","Nyarko","Jesse","Bentil"]
+current_users = ["Prince","Edwin","Nyarko","Jesse","Bentil","kwabena"]
 new_users = ["Abam","Kofi","Yaw","Edwin","Prince"]
+current_names = [current_name.lower() for current_name in current_users]
+
+    
 
 for new_user in new_users:
-    if new_user in current_users:
+    if new_user in current_names:
         print(f"{new_user} Enter new user name")
     else:
         print(f"{new_user} name is available")
+print(current_names)
